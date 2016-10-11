f637f8cc6fc772448fe5118e49aff8b33a200594
Adrián Aguilar Escudero <adri.dev7@gmail.com>
2016-10-11 19:21:18 +0200

    Subiendo el Ejercicio06.java del capítulo04

---
+++ 04_Sentencia_condicional/Ejercicio06.java
@@ -0,0 +1,27 @@
+/**
+* 
+* Realiza un programa que calcule el tiempo que tardará en caer un objeto desde
+* una altura h. Aplica la fórmula t = (√2h/g) siendo g = 9.81 m/s².
+*
+*
+*
+* Condicionales if y switch
+*
+* @author Adrián Aguilar
+*/
+public class Ejercicio06  {
+  public static void main(String[] args) {
+    System.out.print("Por favor, introduzca una altura (en metros): ");
+    Double altura = Double.parseDouble(System.console().readLine());
+    System.out.println(altura + " metros.");
+    double formulaT = Math.sqrt((2*altura)/9.81);
+    
+    if (altura < 0) {
+      System.out.print("\n¡Error!, no hay solución real de la raíz cuadrada de un número negativo.");
+    } else if(altura == 0) {
+      System.out.printf("\nEl objeto tardará en caer " + (int)formulaT + " segundos.");
+    } else {
+      System.out.printf("\nEl objeto tardará en caer %.3f segundos.", formulaT);
+    }
+  }
+}  
