a368d52b8b8b04c87b00ad33bcb4d0da7f7c6533
Adrián Aguilar Escudero <adri.dev7@gmail.com>
2016-10-13 14:02:14 +0200

    Subiendo Ejercicio10.java del capítulo04, y algunos cambios

--- 04_Sentencia_condicional/Ejercicio08.java
+++ 04_Sentencia_condicional/Ejercicio08.java
@@ -26,20 +26,22 @@ public class Ejercicio08  {
     // Establecemos las nuevas condiciones y mostramos las soluciones por pantalla
     if ((nota1 < 0) || (nota2 < 0) || (nota3 < 0)) {
       System.out.println("\n¡Error!, por favor, introduzca una nota válida (no puede ser negativa)."); 
+    } else if ((nota1 > 10) || (nota2 > 10) || (nota3 > 10)) {
+      System.out.println("\n¡Error!, por favor, introduzca una nota válida (no puede ser mayor que 10)."); 
     } else if ((media >= 9) && (media <= 10)) {
-      System.out.printf("\nSu nota media final es de %.2f\n", media);
+      System.out.printf("\nSu nota media final es de %.3f\n", media);
       System.out.print("\n¡Enhorabuena, tiene un sobresaliente!.");
     } else if ((media >= 7) && (media <= 8)) {
-      System.out.printf("\nSu nota media final es de %.2f\n", media);
+      System.out.printf("\nSu nota media final es de %.3f\n", media);
       System.out.print("\nBien hecho, tiene un notable.");
-    } else if (media == 6) {
-      System.out.printf("\nSu nota media final es de %.2f\n", media);
+    } else if (media >= 6) {
+      System.out.printf("\nSu nota media final es de %.3f\n", media);
       System.out.print("\nNo está mal... tiene un bien.");
-    } else if (media == 5) {
-      System.out.printf("\nSu nota media final es de %.2f\n", media);
+    } else if (media >= 5) {
+      System.out.printf("\nSu nota media final es de %.3f\n", media);
       System.out.print("\nTiene un suficiente, ha aprobado por los pelos...");
     } else if ((media < 5) && (media >= 0)) {
-      System.out.printf("\nSu nota media final es de %.2f\n", media);
+      System.out.printf("\nSu nota media final es de %.3f\n", media);
       System.out.print("\nTiene un insuficiente, la próxima vez estudie más...");
     } 
   }
---
+++ 04_Sentencia_condicional/Ejercicio10.java
@@ -0,0 +1,149 @@
+/**
+* 
+* Escribe un programa que nos diga el horóscopo a partir del día y el mes de
+* nacimiento.
+*
+*
+*
+* Condicionales if y switch
+*
+* @author Adrián Aguilar
+*/
+public class Ejercicio10 {
+  public static void main(String[] args) {
+ 
+    // Introducimos los datos por teclado
+    System.out.print("\nPor favor, introduzca su mes de nacimiento: ");
+    int mes = Integer.parseInt(System.console().readLine());
+    System.out.print("Ahora introduzca su día de nacimiento: ");
+    int dia = Integer.parseInt(System.console().readLine());
+    
+    // Establecemos las condiciones y mostramos por pantalla los resultados
+    switch (mes) {
+      case 3:
+        if ((dia > 31) || (dia <= 0)) {
+          System.out.print("\n¡Error!, por favor, introduzca un día válido.");
+        } else if (dia >= 21) {
+          System.out.print("\nSu signo del zodiaco es Aries ♈");
+        } else {
+          System.out.print("\nSu signo del zodiaco es Piscis ♓");
+        } 
+      break;
+      
+      case 4:
+        if ((dia > 31) || (dia <= 0)) {
+          System.out.print("\n¡Error!, por favor, introduzca un día válido.");
+        } else if (dia >= 21) {
+          System.out.print("\nSu signo del zodiaco es Tauro ♉");
+        } else {
+          System.out.print("\nSu signo del zodiaco es Aries ♈");
+        }
+      break;
+      
+      case 5: 
+        if ((dia > 31) || (dia <= 0)) {
+          System.out.print("\n¡Error!, por favor, introduzca un día válido.");
+        } else if (dia >= 22) {
+          System.out.print("\nSu signo del zodiaco es Géminis ♊");
+        } else {
+          System.out.print("\nSu signo del zodiaco es Tauro ♉");
+        }
+      break;
+       
+      case 6:
+        if ((dia > 31) || (dia <= 0)) {
+          System.out.print("\n¡Error!, por favor, introduzca un día válido.");
+        } else if (dia >= 22) {
+          System.out.print("\nSu signo del zodiaco es Cáncer ♋");
+        } else {
+          System.out.print("\nSu signo del zodiaco es Géminis ♊");
+        }
+      break;
+      
+      case 7:
+        if ((dia > 31) || (dia <= 0)) {
+          System.out.print("\n¡Error!, por favor, introduzca un día válido.");
+        } else if (dia >= 23) {
+          System.out.print("\nSu signo del zodiaco es Leo ♌");
+        } else {
+          System.out.print("\nSu signo del zodiaco es Cáncer ♋");
+        }
+      break;
+      
+      case 8:
+        if ((dia > 31) || (dia <= 0)) {
+          System.out.print("\n¡Error!, por favor, introduzca un día válido.");
+        } else if (dia >= 24) {
+          System.out.print("\nSu signo del zodiaco es Virgo ♍");
+        } else {
+          System.out.print("\nSu signo del zodiaco es Leo ♌");
+        }
+      break;
+      
+      case 9:
+        if ((dia > 31) || (dia <= 0)) {
+          System.out.print("\n¡Error!, por favor, introduzca un día válido.");
+        } else if (dia >= 24) {
+          System.out.print("\nSu signo del zodiaco es Libra ♎");
+        } else {
+          System.out.print("\nSu signo del zodiaco es Virgo ♍");
+        }
+      break; 
+      
+      case 10:
+        if ((dia > 31) || (dia <= 0)) {
+          System.out.print("\n¡Error!, por favor, introduzca un día válido.");
+        } else if (dia >= 24) {
+          System.out.print("\nSu signo del zodiaco es Escorpio ♏");
+        } else {
+          System.out.print("\nSu signo del zodiaco es Libra ♎");
+        }
+      break;
+      
+      case 11:
+        if ((dia > 31) || (dia <= 0)) {
+          System.out.print("\n¡Error!, por favor, introduzca un día válido.");
+        } else if (dia >= 23) {
+          System.out.print("\nSu signo del zodiaco es Sagitario ♐");
+        } else {
+          System.out.print("\nSu signo del zodiaco es Escorpio ♏");
+        }
+      break;
+      
+      case 12:
+        if ((dia > 31) || (dia <= 0)) {
+          System.out.print("\n¡Error!, por favor, introduzca un día válido.");
+        } else if (dia >= 22) {
+          System.out.print("\nSu signo del zodiaco es Capricornio ♑");
+        } else {
+          System.out.print("\nSu signo del zodiaco es Sagitario ♐");
+        }
+      break;
+      
+      case 1:
+        if ((dia > 31) || (dia <= 0)) {
+          System.out.print("\n¡Error!, por favor, introduzca un día válido.");
+        } else if (dia >= 21) {
+          System.out.print("\nSu signo del zodiaco es Acuario ♒");
+        } else {
+          System.out.print("\nSu signo del zodiaco es Capricornio ♑");
+        }
+      break;
+      
+      case 2:
+        if ((dia > 31) || (dia <= 0)) {
+          System.out.print("\n¡Error!, por favor, introduzca un día válido.");
+        } else if (dia >= 19) {
+          System.out.print("\nSu signo del zodiaco es Piscis ♓");
+        } else { 
+          System.out.print("\nSu signo del zodiaco es Acuario ♒");
+        }
+      break;
+      default: 
+        System.out.print("\n¡Error!, por favor, introduzca un número de día válido.");
+      
+      
+    }
+  }
+}
+ 
