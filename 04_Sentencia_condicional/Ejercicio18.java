01b5664ed4a2628e0a70acc18dc68a4ef107dc86
Adrián Aguilar Escudero <adri.dev7@gmail.com>
2016-10-18 18:27:45 +0200

    Subiendo Ejercicio18.java del capítulo04.

---
+++ 04_Sentencia_condicional/Ejercicio18.java
@@ -0,0 +1,42 @@
+/**
+* 
+* Escribe un programa que diga cuál es la primera cifra de un número entero
+* introducido por teclado. Se permiten números de hasta 5 cifras.
+*
+*
+*
+* Condicionales if y switch
+*
+* @author Adrián Aguilar
+*/
+public class Ejercicio18 {
+  public static void main(String[] args) {
+
+    System.out.print("\nPor favor, introduzca un número entero por teclado. (Máximo 5 cifras): ");
+    int numero = Integer.parseInt(System.console().readLine());
+    
+    if ((numero > 0) && (numero < 10)) {
+      System.out.print("\nLa primera cifra de " + numero + " es: " + numero);
+    } else if ((numero >= 10) && (numero < 100)) {
+      System.out.print("\nLa primera cifra de " + numero + " es: " + numero / 10);
+    } else if ((numero >= 100) && (numero < 1000)) {
+      System.out.print("\nLa primera cifra de " + numero + " es: " + numero / 100);
+    } else if ((numero >= 1000) && (numero < 10000)) {
+      System.out.print("\nLa primera cifra de " + numero + " es: " + numero / 1000);
+    } else if ((numero >= 10000) && (numero < 100000)) {
+      System.out.print("\nLa primera cifra de " + numero + " es: " + numero / 10000);
+    } else if ((numero < 0) && (numero > -10)) {
+      System.out.print("\nLa primera cifra de " + numero + " es: " + numero);
+    } else if ((numero <= -10) && (numero > -100)) {
+      System.out.print("\nLa primera cifra de " + numero + " es: " + numero / 10);
+    } else if ((numero <= -100) && (numero > -1000)) {
+      System.out.print("\nLa primera cifra de " + numero + " es: " + numero / 100);
+    } else if ((numero <= -1000) && (numero >-10000)) {
+      System.out.print("\nLa primera cifra de " + numero + " es: " + numero / 1000);
+    } else if ((numero <= -10000) && (numero > -100000)) {
+      System.out.print("\nLa primera cifra de " + numero + " es: " + numero / 10000);
+    } else {
+      System.out.print("\n¡Error!, por favor, introduzca un número entero válido. (Máximo 5 cifras).");
+    }
+  }
+}
