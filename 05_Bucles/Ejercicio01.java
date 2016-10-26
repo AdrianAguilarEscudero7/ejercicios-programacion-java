e630986f10ec7bba620b8e9c16fff2f421161d12
Adrián Aguilar Escudero <adri.dev7@gmail.com>
2016-10-26 20:36:52 +0200

    Subiendo Ejercicio01.java del capítulo05

---
+++ 05_Bucles/Ejercicio01.java
@@ -0,0 +1,27 @@
+/**
+* 
+* Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle for.
+*
+*
+*
+* Bucles for, while y do-while.
+*
+* @author Adrián Aguilar
+*/
+
+import java.util.Scanner;
+
+public class Ejercicio01 {
+  public static void main(String[] args) {
+    
+    for (int i = 1; i < 101; i++) {
+      
+      int multiplo = 5;
+      
+      if (i % multiplo == 0) {
+        System.out.println(i);
+      } else {
+      }
+    }
+  }
+}
