3e87eb075d112026db0442f90634f914d136cd87
Adrián Aguilar Escudero <adri.dev7@gmail.com>
2016-10-27 10:00:01 +0200

    Subiendo Ejercicio04.java del capítulo05

---
+++ 05_Bucles/Ejercicio04.java
@@ -0,0 +1,21 @@
+/**
+* 
+* Muestra los números del 320 al 160, contando de 20 en 20 hacia atrás
+* utilizando un bucle for.
+*
+*
+* Bucles for, while y do-while.
+*
+* @author Adrián Aguilar
+*/
+public class Ejercicio04{
+  public static void main(String[] args) {
+      
+    // Creamos el bucle correspondiente
+    for (int i = 320,j = 20; i > 159; i -= j) {
+      
+      System.out.println(i);
+      
+    }
+  }
+}
