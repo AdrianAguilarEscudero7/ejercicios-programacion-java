959232128dc66117836e04bbb2c172b5da590f89
Adrián Aguilar Escudero <adri.dev7@gmail.com>
2016-10-28 16:12:42 +0200

    Algunos cambios realizados

--- 05_Bucles/Ejercicio07.java
+++ 05_Bucles/Ejercicio07.java
@@ -22,7 +22,8 @@ public class Ejercicio07 {
     
     // Definimos las variables
     int claveApertura = 2772;
-    int combinacion = 0;
+    int combinacion;
+    int intentos = 3;
     
     // Presentamos la caja fuerte
     System.out.println("\nBienvenid@ a su caja fuerte");
@@ -38,39 +39,38 @@ public class Ejercicio07 {
     System.out.println("   |                | /");
     System.out.println("   |________________|/");
     
-    // Introducimos por teclado la combinación y mostramos el resultado
-    System.out.print("\nPor favor, introduzca la combinación (4 dígitos): ");
-    combinacion = Integer.parseInt(s.nextLine());
-    if(combinacion == 2772) {
-      System.out.println("\nSu caja fuerte se ha abierto satisfactoriamente.");
-      System.out.println("     ________________");
-      System.out.println("    /_______________/|");
-      System.out.println("    |       /      | |");
-      System.out.println("    |------ | SAFE | |");
-      System.out.println("    |------ |      | |");
-      System.out.println("    |------ |      | |");
-      System.out.println("    |------ |      | |");
-      System.out.println("    |------ | (#)  | |");
-      System.out.println("    |------ |      | /");
-      System.out.println("    |------ |      |/ ");
-      System.out.println("    |------ |     /");
-      System.out.println("    |_______|    /");
-      System.out.println("            |   /");
-      System.out.println("            |  /");
-      System.out.println("            | /");
-      System.out.println("            |/");
+    // Creamos el bucle correspondiente
+    do {     
+      System.out.print("\nPor favor, introduzca la combinación (4 dígitos): ");
+      combinacion = Integer.parseInt(s.nextLine());
       
-      // Si no es cierto, creamos el bucle correspondiente con 3 intentos más
-    } else {
-      for (int i = 1, intentos = 3; i < 4; i++, intentos--) { 
-          System.out.println("\nLo siento... esa no es la combinación correcta, le quedan " + intentos + " intentos más.");
-          System.out.print("\nVuelva a introducir la combinación (4 dígitos): ");
-          combinacion = Integer.parseInt(s.nextLine());
+      // Establecemos las condiciones y mostramos los resultados por pantalla
+      if (combinacion != 2772) {
+        System.out.println("\nLo siento, esa no es la combinación, le quedan " + intentos + " intentos más.");
+        System.out.println("-----------------------------------------------------------------------------");
+        intentos--;
+      } else if (combinacion == 2772) {
+        System.out.println("\nSu caja fuerte se ha abierto satisfactoriamente.");
+        System.out.println("     ________________");
+        System.out.println("    /_______________/|");
+        System.out.println("    |       /      | |");
+        System.out.println("    |------ | SAFE | |");
+        System.out.println("    |------ |      | |");
+        System.out.println("    |------ |      | |");
+        System.out.println("    |------ |      | |");
+        System.out.println("    |------ | (#)  | |");
+        System.out.println("    |------ |      | /");
+        System.out.println("    |------ |      |/ ");
+        System.out.println("    |------ |     /");
+        System.out.println("    |_______|    /");
+        System.out.println("            |   /");
+        System.out.println("            |  /");
+        System.out.println("            | /");
+        System.out.println("            |/");
       }
-      
-      // Si los 4 intentos son erróneos mostramos un mensaje por pantalla
-      System.out.println("-----------------------------------------------------------------------------");
-      System.out.println("\nLa caja fuerte se va a bloquear durante un tiempo, inténtelo más tarde.");
-    }
-  } 
+    } while ((combinacion != 2772) && (intentos >= 0)); 
+    
+  }
 }
+ 
+
