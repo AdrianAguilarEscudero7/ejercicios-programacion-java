4aaff8bd6a768171de375e8f0bd07dd47462aa53
Adrián Aguilar Escudero <adri.dev7@gmail.com>
2016-11-20 22:06:41 +0100

    Subiendo Ejercicio01.java del capítulo06

---
+++ 06_Numeros_aleatorios/Ejercicio01.java
@@ -0,0 +1,33 @@
+/**
+* 
+* Escribe un programa que muestre la tirada de tres dados. Se debe mostrar
+* también la suma total (los puntos que suman entre los tres dados). 
+*
+*
+* Números aleatorios.
+*
+* @author Adrián Aguilar
+*/
+
+import java.util.Scanner;
+
+public class Ejercicio01 {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    
+    System.out.println("\nBienvenid@, este programa mostrará la tirada de 3 dados y realizará la suma total.");
+    System.out.println("-----------------------------------------------------------------------------");
+    System.out.println();
+    int contador = 1;
+    int suma = 0;
+    
+    for (int i = 0; i < 3; i++) {
+      int dado = (int)(Math.random()*6+1);
+      System.out.println("El dado " + contador + " ha sacado: " + dado);
+      suma = suma + dado;
+      contador++;
+      System.out.println();
+    }
+    System.out.println("\nLa suma total de los 3 dados es " + suma);
+  }
+}
