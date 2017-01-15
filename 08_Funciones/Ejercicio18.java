68c108608f719565f04f8427000e81703f7fc8f2
Adrián Aguilar Escudero <adri.dev7@gmail.com>
2017-01-15 20:24:56 +0100

    Subiendo corrección del Ejercicio18.java del capítulo08

--- 08_Funciones/Ejercicio18.java
+++ 08_Funciones/Ejercicio18.java
@@ -22,34 +22,10 @@ public class Ejercicio18 {
     int numeroBase10 = s.nextInt();
     System.out.println();
     
-    int potencia = 0;
-    int contador = 0;
+    // Se realiza la conversión de decimal a binario
+    String numBin = Maths.DecimalBin(numeroBase10);
     
-    // Calcula la potencia en base 2 hasta que sea inmediatamente superior al número decimal introducido
-    do {
-      potencia = Maths.potencia(2,contador);
-      contador++;
-    } while (numeroBase10 / potencia != 0);
-    
-    // Divide la potencia entre 2 para que sea la potencia más alta que quepa en el número en base decimal introducido
-    potencia /= 2;
-
-    System.out.print("El " + numeroBase10 + " convertido a binario es el: ");
-    
-    // Convierte de decimal a binario utilizando el método del residuo
-    if (numeroBase10 == 0) {
-      System.out.print("0");
-    } else {
-      do {
-        if (numeroBase10 / potencia == 1) {
-          System.out.print("1");
-          numeroBase10 -= potencia;
-        } else {
-          System.out.print("0");
-        }
-        potencia /= 2;
-      } while (potencia != 0);
-    }
+    System.out.print("El " + numeroBase10 + " convertido a binario es el: " + numBin);
   }
 }
     
