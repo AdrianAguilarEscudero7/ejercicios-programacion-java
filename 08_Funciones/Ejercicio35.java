6356bb648005dd486c0a0646ca1bec7a9e1c2ace
Adrián Aguilar Escudero <adri.dev7@gmail.com>
2017-01-23 23:45:31 +0100

    Subiendo funciones y Ejercicio36.java del capítulo08

--- 08_Funciones/Ejercicio35.java
+++ 08_Funciones/Ejercicio35.java
@@ -14,13 +14,16 @@ public class Ejercicio35{
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
     
+    // Presenta el programa
     System.out.print("\nBienvenid@ al sistema de palotes, este programa convertirá cualquier número");
     System.out.println(" entero positivo al sistema de palotes.");
     System.out.println("-----------------------------------------------------------------------------\n");
     
+    // Se introducen los datos por teclado
     System.out.print("Por favor, introduzca un número entero positivo: ");
     int numero = s.nextInt();
     
+    // Realiza la conversión del número introducido al sistema de palotes
     System.out.println("\n" + Otros.convierteEnPalotes(numero));
   }
 }
