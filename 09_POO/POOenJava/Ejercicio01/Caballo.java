43a031b61ae5af648a1f4af95133235c043b4634
Adrián Aguilar Escudero <adri.dev7@gmail.com>
2017-01-31 22:41:24 +0100

    Subiendo Ejercicio01 del capítulo09

---
+++ 09_POO/POOenJava/Ejercicio01/Caballo.java
@@ -0,0 +1,89 @@
+  /**
+   * Implementa la clase Caballo vista en un ejercicio anterior. Pruébala creando
+   * instancias y aplicándole algunos métodos.
+   * 
+   * @author Adrián Aguilar
+   */
+package ejercicio01;
+
+public class Caballo {
+
+  public enum Sexo {
+    HEMBRA, MACHO
+  }
+
+  private Sexo sexo;
+  private String raza;
+  private int edad;
+  private int altura;
+  private String temperamento;
+  
+  public Caballo() {
+    this.sexo = Sexo.MACHO;
+    this.raza = "Mustang";
+    this.edad = 15;
+    this.altura = 170;
+    this.temperamento = "Nervioso";
+  }
+  public Caballo(Sexo s, String r, int e, int a, String temp) {
+    this.sexo = s;
+    this.raza = r;
+    this.edad = e;
+    this.altura = a;
+    this.temperamento = temp;
+  }
+  public Caballo(Sexo s) {
+    this.sexo = s;
+    this.raza = "Mustang";
+    this.edad = 15;
+    this.altura = 170;
+    this.temperamento = "Nervioso";
+  }
+  public String toString() {
+    return "\n****************************"
+        + "\nSexo: " + this.sexo
+        + "\n" + "Raza: " + this.raza
+        + "\n" + "Edad: " + this.edad
+        + "\n" + "Altura: " + this.altura
+        + "\n" + "Temperamento: " + this.temperamento
+        + "\n****************************\n";
+  }
+  public void come(String c) {
+    if (c.equals("vegetales")) {
+      System.out.println("\nGracias, esto está muy bueno.");
+    } else {
+      System.out.println("\nLo siento, yo soy hervíbor@, solo como vegetales.");
+    }
+  }
+  public void bebe() {
+    System.out.println("\nEstoy bebiendo un poco de agua para bajar la comida, no moleste.");
+  }
+  public void duerme() {
+    System.out.println("\nzZZzZzZzZzZzZ");
+  }
+  public void pasea() {
+    System.out.println("\nEstoy paseando tranquilamente...");
+  }
+  public void trota() {
+    System.out.println("\nEstoy trotando...");
+  }
+  public void galopa() {
+    System.out.println("\n¡Estoooooy gaaalopaaandoooo!...");
+  }
+  public void seReproduce() {
+    System.out.println("\nShh, ahora necesito intimidad, fuera.");
+  }
+  public void seReproduceCon(Caballo pareja) {
+    if ((this.sexo == Sexo.MACHO) && (pareja.sexo == Sexo.HEMBRA)) {
+      System.out.println("\nVen paquí moza que te empotro.");
+    } else if ((this.sexo == Sexo.HEMBRA) && (pareja.sexo == Sexo.MACHO)) {
+      System.out.println("\nHola guapo, ¿quieres pasar un buen rato?.");
+    } else {
+      System.out.println("\nLo siento aún no podemos decidir ser homosexuales =S.");
+    }
+  }
+  public void relincha() {
+    System.out.println("\nHhIIIhhbrfff");
+  }
+}
+
---
+++ 09_POO/POOenJava/Ejercicio01/PruebaCaballo.java
@@ -0,0 +1,47 @@
+/**
+ * Implementa la clase Caballo vista en un ejercicio anterior. Pruébala creando
+ * instancias y aplicándole algunos métodos.
+ * 
+ * @author Adrián Aguilar
+ */
+package ejercicio01;
+import java.util.Scanner;
+
+import ejercicio01.Caballo.Sexo;
+
+public class PruebaCaballo {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    Scanner t = new Scanner(System.in);
+    
+    System.out.println("\nIntroduzca los siguientes campos para el caballo Benji.");
+    System.out.print("Raza: ");
+    String raza = s.nextLine();
+    System.out.print("\nEdad: ");
+    int edad = s.nextInt();
+    System.out.print("\nAltura (en cm): ");
+    int altura = s.nextInt();
+    System.out.print("\nTemperamento: ");
+    String temp = t.nextLine();
+    
+    Caballo benji = new Caballo(Sexo.MACHO,raza,edad,altura,temp);
+    Caballo yesi = new Caballo(Sexo.HEMBRA);
+    Caballo musti = new Caballo();
+    
+    System.out.println(benji);
+    System.out.println(yesi);
+    System.out.println(musti);
+    
+    yesi.duerme();
+    benji.come("carne");
+    musti.come("vegetales");
+    musti.galopa();
+    benji.bebe();
+    yesi.trota();
+    benji.pasea();
+    yesi.seReproduceCon(musti);
+    musti.seReproduce();
+    benji.seReproduceCon(musti);
+    musti.relincha();
+  }
+}
