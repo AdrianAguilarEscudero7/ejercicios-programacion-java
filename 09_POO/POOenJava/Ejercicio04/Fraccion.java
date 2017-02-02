53731cde4893aec1788dd1bb8fcc949114f6ac2c
Adrián Aguilar Escudero <adri.dev7@gmail.com>
2017-02-02 17:24:00 +0100

    Subiendo Ejercicio04 del capítulo09

---
+++ 09_POO/POOenJava/Ejercicio04/Fraccion.java
@@ -0,0 +1,49 @@
+/**
+ * Crea la clase Fracción. Los atributos serán numerador y denominador. Y algunos de
+ * los métodos pueden ser invierte, simplifica, multiplica, divide, etc.
+ * 
+ * @author Adrián Aguilar
+ */
+package ejercicio04;
+
+public class Fraccion {
+
+  private int numerador;
+  private int denominador;
+  
+  public Fraccion(int n, int d) {
+    this.numerador = n;
+    this.denominador = d;
+  }
+  
+  @Override
+  public String toString() {
+    return this.numerador + "/" + this.denominador;
+  }
+  
+  public void invierte() {
+    int aux = this.numerador;
+    this.numerador = this.denominador;
+    this.denominador = aux;
+  }
+  
+  public Fraccion multiplica(Fraccion n) {
+    return new Fraccion(this.numerador * n.numerador, this.denominador * n.denominador);
+  }
+  
+  public Fraccion simplifica() {
+    int mcd = 1;
+    int minimo = Math.min(this.numerador, this.denominador);
+    
+    for (int i = 2; i <= minimo; i++) {
+      if ((this.numerador % i == 0) && (this.denominador % i == 0)) {
+        mcd = i;
+      }
+    }
+    return new Fraccion(this.numerador/mcd, this.denominador/mcd);
+  }
+  
+  public Fraccion divide(Fraccion n) {
+    return new Fraccion(this.numerador * n.denominador, this.denominador * n.numerador);
+  }
+}
---
+++ 09_POO/POOenJava/Ejercicio04/PruebaFraccion.java
@@ -0,0 +1,28 @@
+/**
+ * Crea la clase Fracción. Los atributos serán numerador y denominador. Y algunos de
+ * los métodos pueden ser invierte, simplifica, multiplica, divide, etc.
+ * 
+ * @author Adrián Aguilar
+ */
+package ejercicio04;
+
+public class PruebaFraccion {
+  public static void main(String[] args) {
+    
+    Fraccion f1 = new Fraccion(24,32);
+    Fraccion f2 = new Fraccion(6,4);
+    Fraccion f3 = new Fraccion(146,128);
+    
+    System.out.println(f1);
+    
+    System.out.println("\n" + f2);
+    
+    System.out.println("\n" + f1.multiplica(f2));
+    
+    System.out.println("\n" + f1.divide(f2));
+    
+    System.out.println("\n" + f3);
+    
+    System.out.println("\n" + f3.simplifica());
+  }
+}
